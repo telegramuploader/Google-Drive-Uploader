774d6f8443a10c794ffdfa65cf14473c87bad3a2
telegramuploader <64506670+telegramuploader@users.noreply.github.com>
2021-03-31 20:24:19 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -8,10 +8,10 @@ class Creds():
     # Because of pydrive And pydrive v2 Api
     
     #Folder Id Of Teamdrive
-    TEAMDRIVE_FOLDER_ID = ""
+    TEAMDRIVE_FOLDER_ID = "1K2zdjYWUeKtYV3yxP6y86nyLgHb1xTFa"
     
     # Id of Team drive 
-    TEAMDRIVE_ID = ""
+    TEAMDRIVE_ID = "0ACE4xLtRSW4hUk9PVA"
     
     
     
